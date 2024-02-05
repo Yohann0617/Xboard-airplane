9bcb3a83faf5ba86ce9619a96308304a1a06b653
Yohann <75626191+Yohann0617@users.noreply.github.com>
2024-02-05 14:45:19 +0800

    Update SingBox.php

--- app/Protocols/SingBox.php
+++ app/Protocols/SingBox.php
@@ -185,7 +185,7 @@ class SingBox
             "server" => $server['host'],
             "server_port" => $server['port'],
             "uuid" => $password,
-            "packet_encoding" => "xudp"
+            "packet_encoding" => ""
         ];
 
         $tlsSettings = $server['tls_settings'] ?? [];
@@ -266,16 +266,16 @@ class SingBox
         if(isset($server['network']) && in_array($server['network'], ["grpc", "ws"])){
             $array['transport']['type'] = $server['network'];
             // grpc配置
-            if($server['network'] === "grpc" && isset($server['network_settings']['serviceName'])) {
-                $array['transport']['service_name'] = $server['network_settings']['serviceName'];
+            if($server['network'] === "grpc" && isset($server['networkSettings']['serviceName'])) {
+                $array['transport']['service_name'] = $server['networkSettings']['serviceName'];
             }
             // ws配置
             if($server['network'] === "ws") {
-                if(isset($server['network_settings']['path'])) {
-                    $array['transport']['path'] = $server['network_settings']['path'];
+                if(isset($server['networkSettings']['path'])) {
+                    $array['transport']['path'] = $server['networkSettings']['path'];
                 }
-                if(isset($server['network_settings']['headers']['Host'])){
-                    $array['transport']['headers'] = ['Host' => array($server['network_settings']['headers']['Host'])];
+                if(isset($server['networkSettings']['headers']['Host'])){
+                    $array['transport']['headers'] = ['Host' => array($server['networkSettings']['headers']['Host'])];
                 }
                 $array['transport']['max_early_data'] = 2048;
                 $array['transport']['early_data_header_name'] = 'Sec-WebSocket-Protocol';
